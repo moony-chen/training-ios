8202f4912999a39b7c24b26d5be42c198117ff69
moony.chen <moony.chen@perficient.com>
2020-07-22 14:43:54 +0800

    Initial Commit

---
+++ training-ios-new/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  training-ios-new
+//
+//  Created by Moony Chen on 2020/7/22.
+//  Copyright © 2020 Moony Chen. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
